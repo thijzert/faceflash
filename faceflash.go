23a76dab2a3d1306f30046384661d9d7b4bccbdd
Thijs van Dijk <github@inurbanus.nl>
2014-01-07 16:50:58 +0100

    Read a folder with images

--- faceflash.go
+++ faceflash.go
@@ -2,16 +2,68 @@
 package main
 
 import (
-    "github.com/hoisie/web"
-    "io/ioutil"
-    "regexp"
-    "mime"
-    "path/filepath"
+	"crypto/sha1"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"mime"
+	"path/filepath"
+	"regexp"
+
+	"github.com/hoisie/web"
 )
 
 
+type FaceMap map[string]Face
 
-var dots = regexp.MustCompile( "\\.+" );
+type Face struct {
+	filename string
+	Names []string
+}
+
+
+func ( fm FaceMap ) ParseDir( dir string ) {
+	files, err := ioutil.ReadDir( dir )
+	if err != nil { return }
+
+	for _, file := range files {
+		if file.IsDir() { continue }
+		sha := sha1sum( dir + "/" + file.Name() )
+		ff, ok := fm[sha]
+		if ok {
+			ff.AppendName( file.Name() )
+		} else {
+			fm[sha] = Face{ dir + "/" + file.Name(), []string{ file.Name() } }
+		}
+	}
+}
+func ( ff Face ) SetFile( file string ) {
+	ff.filename = file
+}
+func ( ff Face ) AppendName( name string ) {
+	n := len(ff.Names)
+	if n == cap(ff.Names) {
+		// Slice is full; must grow.
+		// We double its size and add 1, so if the size is zero we still grow.
+		newSlice := make([]string, len(ff.Names), 2*len(ff.Names)+1)
+		copy(newSlice, ff.Names)
+		ff.Names = newSlice
+	}
+	ff.Names = ff.Names[0 : n+1]
+	ff.Names[n] = name
+}
+
+
+func sha1sum( filename string ) string {
+	s := sha1.New()
+	b, err := ioutil.ReadFile( filename )
+	if err != nil { return "" }
+	s.Write( b )
+	return fmt.Sprintf( "%x", s.Sum( nil ) )
+}
+
+
+var dots = regexp.MustCompile( "\\.+" )
 
 func read_asset( ctx *web.Context, filename string )  {
 
@@ -28,10 +80,24 @@ func read_asset( ctx *web.Context, filename string )  {
 	}
 }
 
+
+
+var ImageFolder = "/path/to/images"
+
 func main() {
 	s := web.NewServer()
 
+	fm := make(FaceMap)
+	fm.ParseDir( ImageFolder )
+
 	s.Get( "/assets/(.*)", read_asset );
+
+	s.Get( "/faces.json", func( ctx *web.Context ) {
+		ctx.SetHeader( "Content-type", "application/json", true );
+		js, _ := json.Marshal( fm )
+		ctx.Write( js )
+	} )
+
 	s.Run( "127.0.0.1:9999" );
 }
 
